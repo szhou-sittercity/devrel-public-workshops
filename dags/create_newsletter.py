9cd1639f07e3887b8a64e1a2fe2b1f57c1f951d7
szhou-sittercity <105938216+szhou-sittercity@users.noreply.github.com>
2025-08-07 15:53:45 -0500

    Update create_newsletter.py

--- dags/create_newsletter.py
+++ dags/create_newsletter.py
@@ -19,7 +19,7 @@ def raw_zen_quotes(context: dict):
     """
     import requests
 
-    r = requests.get("https://zenquotes.io/api/quotes/random")
+    r = requests.get("https://zenquotes.io/api/quotes")
     quotes = r.json()
 
     run_date = context["dag_run"].logical_date.strftime("%Y-%m-%d")
